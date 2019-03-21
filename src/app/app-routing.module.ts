06222c6e8bbcebf4e322af8517854fcc287e9f8c
Robert Tyrees <robert.tyree@stinkstudios.com>
2019-03-21 17:31:06 +0000

    add egghead course code and notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { DynamicFormComponent } from './e-dynamic-form/e-dynamic-form-component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'input', component: NameEditorComponent },
-  { path: 'form', component: ProfileEditorComponent }
+  { path: 'form', component: ProfileEditorComponent },
+  { path: 'e-form', component: DynamicFormComponent }
 ];
 
 @NgModule({
